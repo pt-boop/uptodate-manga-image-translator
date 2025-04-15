c80bff378f7db81b900b4674b16ab387cc3de2f6
DuckDuckGo-prog <weql4143@duck.com>
2025-04-16 02:40:21 +0800

    Update groq.py loosen to temp 0.4
    Update groq.py loosen to temp 0.4

--- manga_translator/translators/groq.py
+++ manga_translator/translators/groq.py
@@ -98,7 +98,7 @@ class GroqTranslator(CommonTranslator):
 
     @property
     def temperature(self) -> float:
-        return self._config_get('temperature', default=0.2)
+        return self._config_get('temperature', default=0.4)
     
     @property
     def top_p(self) -> float:
