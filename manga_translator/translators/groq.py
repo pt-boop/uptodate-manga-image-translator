0ab543e40997e5d8ffdc05b0171313097083a6e3
DuckDuckGo-prog <weql4143@duck.com>
2025-04-18 00:51:24 +0800

    Update groq.py adjust topp
    Update groq.py adjust topp

--- manga_translator/translators/groq.py
+++ manga_translator/translators/groq.py
@@ -101,7 +101,7 @@ class GroqTranslator(CommonTranslator):
     
     @property
     def top_p(self) -> float:
-        return self._config_get('top_p', default=1)
+        return self._config_get('top_p', default=0.95)
 
     def _format_prompt_log(self, to_lang: str, prompt: str) -> str:
         return '\n'.join([
